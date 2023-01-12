f442dec15cfa0e892e914601e15ee46abe9c45b1
vityaman <vityaman.dev@yandex.ru>
2023-01-12 11:57:22 +0300

    added expr types for reflection

--- src/expression.h
+++ src/expression.h
@@ -3,19 +3,39 @@
 
 #include <string>
 
+enum expression_type {
+    IMPLICATION,
+    DISJUNCTION,
+    CONJUNCTION,
+    NEGATION,
+    VARIABLE
+};
+
 class expression {
 public:
+    expression(expression_type type)
+        : _type(type) {}
+
+    expression_type type() const {
+        return _type;
+    }
+
     virtual std::string as_string() = 0;
     virtual ~expression() {};
+
+private:
+    expression_type _type;
 };
 
 class binary_operation : public expression {
 public:
     binary_operation(
         const std::string& symbol,
+        expression_type type,
         expression* left,
         expression* right
-    ) : _symbol(symbol), 
+    ) : expression(type), 
+        _symbol(symbol), 
         _left(left),
         _right(right) {}
 
@@ -36,26 +56,30 @@ private:
 class implication : public binary_operation {
 public:
     implication(expression* left, expression* right) 
-        : binary_operation("->", left, right) {}
+        : binary_operation("->", IMPLICATION, left, right) {}
 };
 
 class disjunction : public binary_operation {
 public:
     disjunction(expression* left, expression* right) 
-        : binary_operation("|", left, right) {}
+        : binary_operation("|", DISJUNCTION, left, right) {}
 };
 
 class conjunction : public binary_operation {
 public:
     conjunction(expression* left, expression* right) 
-        : binary_operation("&", left, right) {}
+        : binary_operation("&", CONJUNCTION, left, right) {}
 };
 
 class unary_operation : public expression {
 public:
-    unary_operation(std::string const & symbol, expression* child) 
-        : _symbol(symbol),
-          _child(child) {}
+    unary_operation(
+        std::string const & symbol, 
+        expression_type type,
+        expression* child
+    ) : expression(type),
+        _symbol(symbol),
+        _child(child) {}
 
     inline std::string as_string() override {
         return "(" 
@@ -72,13 +96,14 @@ private:
 class negation : public unary_operation {
 public:
     negation(expression* child) 
-        : unary_operation("!", child) {}
+        : unary_operation("!", NEGATION, child) {}
 };
 
 class variable : public expression {
 public:
     variable(const std::string& name)
-        : _name(name) {}
+        : expression(VARIABLE),
+          _name(name) {}
 
     inline std::string as_string() override {
         return _name;
